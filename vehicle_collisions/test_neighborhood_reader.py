b0fed63095c4db84e9d13317f841bf9633ff1b13
SalikWarsi <salikwarsi@gmail.com>
2018-05-12 23:00:21 -0700

    Add component to read neighborhood data and assing neighboorhood to collision data. Also added an example in how to use it and 1 test

---
+++ vehicle_collisions/test_neighborhood_reader.py
@@ -0,0 +1,23 @@
+"""
+Unittests for neighborhood_reader.py
+"""
+
+import unittest
+from neighborhood_reader import assign_neighborhood
+from neighborhood_reader import get_neighborhood
+
+# Define a class in which the tests will run
+class NeighborhoodReaderTest(unittest.TestCase):
+    """
+    Unittests for neighborhood_reader
+    """
+
+    def test_wrong_area(self):
+        """
+        Tests that wrong location returns -1
+        """
+        object_id = get_neighborhood(0, 0)
+        self.assertTrue(object_id == -1)
+
+if __name__ == '__main__':
+    unittest.main()

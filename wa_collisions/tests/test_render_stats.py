06afefaadd30eadb0af5e5a07b4a12b70e7c7d30
gdc3000 <geoffcoyner@gmail.com>
2018-06-04 21:39:19 -0700

    Added unit test to run causal impact.

--- wa_collisions/tests/test_render_stats.py
+++ wa_collisions/tests/test_render_stats.py
@@ -3,6 +3,8 @@ Unittests for render_stats.py
 """
 import datetime
 import unittest
+import numpy as np
+import pandas as pd
 from causalimpact import CausalImpact
 
 import wa_collisions.render_stats as render_stats
@@ -250,9 +252,9 @@ class RenderStatsTest(unittest.TestCase):
         max_date = datetime.date(max_date.year, max_date.month, max_date.day)
         self.assertTrue(max_date.strftime('%Y-%m-%d') == out[1][1])
 
-    def test_causal_impact(self):
+    def test_causal_impact_load(self):
         """
-        Performs a smoke test using causal impact to ensure jupyter notebook will run.
+        Performs a smoke test where we load causal impact.
         """
         #Setup
         test_treatment_in = ['Atlantic', 'Pike-Market', 'Belltown', 'International District'
@@ -267,6 +269,24 @@ class RenderStatsTest(unittest.TestCase):
         causal_impact_out = CausalImpact(out_df, out_date[0], out_date[1])
 
         self.assertTrue(isinstance(causal_impact_out, CausalImpact))
+
+    def test_causal_impact_run(self):
+        """
+        Performs a smoke test where we run causal impact on test data. This
+            test will validate Jupyter notebook dependencies.
+        """
+        try:
+            df = pd.date_range(start='1/1/2017', end='1/08/2018')
+            df = df.to_frame()
+            df['SpeedLimitChange'] = np.random.normal(5, 1)
+            df['SpeedLimitSame'] = np.random.normal(10, 2)
+            df = df.drop([0], axis=1)
+
+            impact_test = CausalImpact(df, ['2017-01-01', '2017-06-01']
+                                       , ['2017-06-02', '2018-01-08'])
+            impact_test.run()
+        except KeyError:
+            self.fail("test_causal_impact_run raised a KeyError.")
 
 if __name__ == '__main__':
     unittest.main()
